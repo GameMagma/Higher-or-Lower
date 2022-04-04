08e9b2ce697ecffa6a12976dd300e17e46652750
Connor Midgley <53975931+GameMagma@users.noreply.github.com>
2022-04-04 08:51:35 -0500

    Minor Cleanup
    Separate user's input and the standardized answer. Fix typo, close scanner, tiny changes

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         /* Ask player what their choice is */
         System.out.print("Your card is the " + playerCard.getName() + ". Is the AI's card Higher or Lower? Enter your choice now: ");
-        String choice = sc.next().toLowerCase(); // Makes the whole thing lowercase to ignore the casing it was entered as
+        String input = sc.next().toLowerCase(); // Makes the whole thing lowercase to ignore the casing it was entered as
         System.out.println(); // Moves to next line
 
         /* Get and compare card values */
@@ -23,26 +23,27 @@ public class Main {
         int correctAnswer; // Results of the comparison, in int form (to make it easier to test against)
         int playerAnswer = 0; // The player's answer, converted to int
 
-        // Determine player answer and reassign choice to use later. Little icky, but whatever
-        if (choice.equals("higher") || choice.equals("h") || choice.equals("high")) {
+        // Look for valid player input, change to the full word to make sure it prints correctly later
+        String choice; // Standardized choice
+        if (input.equals("higher") || input.equals("h") || input.equals("high")) {
             playerAnswer = 3;
             choice = "higher";
         }
-        else if (choice.equals("lower") || choice.equals("l") || choice.equals("low")) {
+        else if (input.equals("lower") || input.equals("l") || input.equals("low")) {
             playerAnswer = 1;
             choice = "lower";
         }
         else { // If they didn't enter any of these, then we don't know what they wanted, so just stop the program.
             System.out.println("Invalid entry entered. Stopping program.");
             System.exit(-1); // Exits with error code -1, signaling something went wrong.
-        } // If I wanted to do a little more, I would suggest putting this in a loop so they could keep trying, or even
+        } // If I wanted to do a little more, I would suggest putting this in a loop, so they could keep trying, or even
         // doing error handling. But I doubt that it's going to matter, unless people want to do it just to learn.
 
         // Determine correct answer
         if (aiCard.getValue() > playerCard.getValue()) { correctAnswer = 3; } // Higher
         else if (aiCard.getValue() < playerCard.getValue()) { correctAnswer = 1; } // Lower
-        else { correctAnswer = 2; } /* Tie - If we wanted, we wouldn't even need to assign this to something here. Just
-                                       say it's a tie and end the program. */
+        else { correctAnswer = 2; } // Tie - If we wanted, we wouldn't even need to assign this to something here. Just
+                                    // say it's a tie and end the program.
 
 
         /* Tell player if they've won or not */
@@ -58,5 +59,7 @@ public class Main {
         } else {
             System.out.println("Sorry, you lost. Better luck next time.");
         }
+
+        sc.close();
     }
 }
